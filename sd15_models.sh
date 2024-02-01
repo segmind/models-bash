c66a5a99ba40779da6f8e777c61920703340dd14
Ubuntu <ubuntu@ip-172-31-66-201.ec2.internal>
2024-02-01 12:24:51 +0000

    sd 1.5 models bash

---
+++ sd15_models.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -O "/segmind/stable-diffusion-webui/models/Stable-diffusion/juggernaut_15.safetensors" https://civitai.com/api/download/models/274039 --content-disposition
+wget -O "/segmind/stable-diffusion-webui/models/Stable-diffusion/cyberrealistic_v4_15.safetensors" https://civitai.com/api/download/models/198401 --content-disposition
+wget -O "/segmind/stable-diffusion-webui/models/Stable-diffusion/epic_realisim_15.safetensors" https://civitai.com/api/download/models/160989 --content-disposition
+wget -O "/segmind/stable-diffusion-webui/models/Stable-diffusion/realistic_vision_v5-1_15.safetensors" https://civitai.com/api/download/models/130072 --content-disposition
