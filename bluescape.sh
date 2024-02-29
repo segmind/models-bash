639743bc87ff43a5b444e1a18dc13d8f1ac9816c
Sumanth Donapati <74263775+sumanthdonapati@users.noreply.github.com>
2024-02-29 22:08:20 +0530

    Add files via upload

---
+++ bluescape.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O "/segmind/stable-diffusion-webui/models/Stable-diffusion/sdxl_base_1-0.safetensors" https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors
+
+wget -O "/segmind/stable-diffusion-webui/models/Stable-diffusion/sd_xl_refiner_1.0.safetensors" https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors
