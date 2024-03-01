ef4c13192971495f04c3e836f843182bef9a9daa
Sumanth Donapati <74263775+sumanthdonapati@users.noreply.github.com>
2024-03-01 18:18:58 +0530

    Create comfy_lightning_models.sh

---
+++ comfy_lightning_models_1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget -O "/segmind/ComfyUI/models/checkpoints/Juggernaut_XL_Lightning_V9.safetensors" https://civitai.com/api/download/models/357609 --content-disposition
+wget -O "/segmind/ComfyUI/models/checkpoints/RealVis_XL_Lightning_V4.safetensors" https://civitai.com/api/download/models/361593 --content-disposition
+wget -O "/segmind/ComfyUI/models/checkpoints/WildCard_XL_Lightning.safetensors" https://civitai.com/api/download/models/360292 --content-disposition
+wget -O "/segmind/ComfyUI/models/checkpoints/DreamShaper_XL_Lightning.safetensors" https://civitai.com/api/download/models/354657 --content-disposition
