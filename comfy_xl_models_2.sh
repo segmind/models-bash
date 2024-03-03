83b3f80e868fc32ebd1a8cd1352369fe2eed0710
Sumanth Donapati <74263775+sumanthdonapati@users.noreply.github.com>
2024-03-03 13:29:32 +0530

    Create comfy_xl_models_2.sh

---
+++ comfy_xl_models_2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget -O "/segmind/ComfyUI/models/checkpoints/ColossusProject_XL_V6.safetensors" https://civitai.com/api/download/models/355884 --content-disposition
+wget -O "/segmind/ComfyUI/models/checkpoints/Samaritan_3D_XL_V4.safetensors" https://civitai.com/api/download/models/144566 --content-disposition
+wget -O "/segmind/ComfyUI/models/checkpoints/Animagine_XL_V3.safetensors" https://civitai.com/api/download/models/293564 --content-disposition
+wget -O "/segmind/ComfyUI/models/checkpoints/WildCardX_XL_V1.safetensors" https://civitai.com/api/download/models/334225 --content-disposition
+wget -O "/segmind/ComfyUI/models/checkpoints/NightVision_XL_V8.safetensors" https://civitai.com/api/download/models/343830 --content-disposition
