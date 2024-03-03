0e72b119964bac1d2c51ce32fc95ffc15d1c8f0e
Sumanth Donapati <74263775+sumanthdonapati@users.noreply.github.com>
2024-03-03 13:17:33 +0530

    Create comfy_xl_models_1.sh

---
+++ comfy_xl_models_1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -O "/segmind/ComfyUI/models/checkpoints/Juggernaut_XL_V8.safetensors" https://civitai.com/api/download/models/348913 --content-disposition
+wget -O "/segmind/ComfyUI/models/checkpoints/RealVis_XL_V4.safetensors" https://civitai.com/api/download/models/344487 --content-disposition
+wget -O "/segmind/ComfyUI/models/checkpoints/RealismEngine_XL_V3.safetensors" https://civitai.com/api/download/models/293240 --content-disposition
+wget -O "/segmind/ComfyUI/models/checkpoints/DreamShaper_XL_V2.safetensors" https://civitai.com/api/download/models/126688 --content-disposition
+wget -O "/segmind/ComfyUI/models/checkpoints/ProtoVision_XL_V6_6.safetensors" https://civitai.com/api/download/models/265938 --content-disposition
+wget -O "/segmind/ComfyUI/models/checkpoints/ProtoVision_XL_V6_6.safetensors" https://civitai.com/api/download/models/265938 --content-disposition
