55830e851d3ff9bdca5e445146fc876568c4165f
Sumanth Donapati <74263775+sumanthdonapati@users.noreply.github.com>
2024-03-05 15:32:29 +0530

    Create comfy_lightning_models_3.sh

---
+++ comfy_lightning_models_3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /segmind/ComfyUI/models/checkpoints/
+
+wget https://huggingface.co/kg-09/lightning_models/resolve/main/nightvis_lightning.safetensors
+wget https://huggingface.co/kg-09/lightning_models/resolve/main/protovis_lightning.safetensors
+wget https://huggingface.co/kg-09/lightning_models/resolve/main/realism_lightning.safetensors
+wget https://huggingface.co/kg-09/lightning_models/resolve/main/samaritan_lightning.safetensors
+
+cd /segmind/
