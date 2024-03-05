ac3cfad3c0eb49d58b29187695dafa7db3043327
Sumanth Donapati <74263775+sumanthdonapati@users.noreply.github.com>
2024-03-05 15:33:45 +0530

    Nightvis, Protovis, Samartin, Realism

--- comfy_lightning_models_3.sh
+++ comfy_lightning_models_3.sh
@@ -7,4 +7,5 @@ wget https://huggingface.co/kg-09/lightning_models/resolve/main/protovis_lightni
 wget https://huggingface.co/kg-09/lightning_models/resolve/main/realism_lightning.safetensors
 wget https://huggingface.co/kg-09/lightning_models/resolve/main/samaritan_lightning.safetensors
 
+
 cd /segmind/
