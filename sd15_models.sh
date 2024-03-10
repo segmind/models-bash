f57d7d5ed3be2f1cc2c8a2b32aa9dcb22d15af46
Sumanth Donapati <74263775+sumanthdonapati@users.noreply.github.com>
2024-03-10 14:33:24 +0530

    Update sd15_models.sh
    added sd1.5 realistic vision

--- sd15_models.sh
+++ sd15_models.sh
@@ -5,4 +5,5 @@ wget -O "/segmind/stable-diffusion-webui/models/Stable-diffusion/cyberrealistic_
 wget -O "/segmind/stable-diffusion-webui/models/Stable-diffusion/epic_realisim_15.safetensors" https://civitai.com/api/download/models/160989 --content-disposition
 wget -O "/segmind/stable-diffusion-webui/models/Stable-diffusion/realistic_vision_v5-1_15.safetensors" https://civitai.com/api/download/models/130072 --content-disposition
 wget -O "/segmind/stable-diffusion-webui/models/Stable-diffusion/edgeofrealism-1_15.safetensors" https://civitai.com/api/download/models/51913  --content-disposition
+wget -O "/segmind/stable-diffusion-webui/models/Stable-diffusion/realisticvision_v3.safetensors" https://civitai.com/api/download/models/130072?type=Model&format=SafeTensor&size=pruned&fp=fp16  --content-disposition
 wget -O "/segmind/stable-diffusion-webui/models/Stable-diffusion/reliberate_v2-1_15.safetensors" https://huggingface.co/XpucT/Reliberate/resolve/main/Reliberate_v2.safetensors
