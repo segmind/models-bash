65dcde97e14dbe353203e6327ef00e8a5a330e0e
Sumanth Donapati <74263775+sumanthdonapati@users.noreply.github.com>
2024-03-14 14:25:02 +0530

    Update sdxl_models.sh

--- sdxl_models.sh
+++ sdxl_models.sh
@@ -3,5 +3,9 @@
 cd /segmind/ComfyUI/models/checkpoints/
 
 wget -O dreamshaper_xl.safetensors https://civitai.com/api/download/models/351306
+wget -O realvis_xl.safetensors https://civitai.com/api/download/models/361593
+wget -O timeless_xl.safetensors https://civitai.com/api/download/models/344540
+wget -O zavychroma_xl.safetensors https://civitai.com/api/download/models/362861
+wget -O samaritan_xl.safetensors https://civitai.com/api/download/models/144566
 
 cd /segmind/
