9980b910a27207624cc761bc00899928da068017
Sumanth Donapati <74263775+sumanthdonapati@users.noreply.github.com>
2024-03-15 10:50:06 +0530

    Update auto_sdxl_models.sh

--- auto_sdxl_models.sh
+++ auto_sdxl_models.sh
@@ -2,8 +2,8 @@
 
 cd /segmind/stable-diffusion-webui/models/Stable-diffusion/
 
-wget -O dreamshaper_xl.safetensors https://civitai.com/api/download/models/351306
-wget -O realvis_xl.safetensors https://civitai.com/api/download/models/361593
+wget -O dreamshaper_xl.safetensors https://civitai.com/api/download/models/126688
+wget -O realvis_xl.safetensors https://civitai.com/api/download/models/344487
 wget -O timeless_xl.safetensors https://civitai.com/api/download/models/344540
 wget -O zavychroma_xl.safetensors https://civitai.com/api/download/models/362861
 wget -O samaritan_xl.safetensors https://civitai.com/api/download/models/144566
